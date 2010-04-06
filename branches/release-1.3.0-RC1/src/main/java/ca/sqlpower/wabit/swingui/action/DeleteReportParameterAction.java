a806d8194fc5f351a79d7bcdd5037722b5c2507c
lucboudreau <lucboudreau@78c4bb32-93da-11dd-ada7-8df265a40b68>
2010-04-06 14:13:23 +0000

    Creates the RC1 branch

---
+++ branches/release-1.3.0-RC1/src/main/java/ca/sqlpower/wabit/swingui/action/DeleteReportParameterAction.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2010, SQL Power Group Inc.
+ *
+ * This file is part of Wabit.
+ *
+ * Wabit is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Wabit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+package ca.sqlpower.wabit.swingui.action;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+
+import ca.sqlpower.wabit.report.selectors.Selector;
+import ca.sqlpower.wabit.swingui.WabitIcons;
+
+public class DeleteReportParameterAction extends AbstractAction {
+
+	private final Component dialogOwner;
+	private final Selector selector;
+	private final Runnable reportRefresher;
+
+	public DeleteReportParameterAction(
+			Component dialogOwner,
+			Selector selector, 
+			Runnable reportRefresher) 
+	{
+		super("", WabitIcons.DELETE_12);
+		this.dialogOwner = dialogOwner;
+		this.selector = selector;
+		this.reportRefresher = reportRefresher;
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		
+		int result = 
+				JOptionPane.showConfirmDialog(
+						dialogOwner, 
+						"Delete this parameter?",
+						"",
+						JOptionPane.YES_NO_OPTION);
+		
+		if (result == JOptionPane.YES_OPTION) {
+			try {
+				selector.cleanup();
+				selector.getParent().removeChild(selector);
+				SwingUtilities.invokeLater(reportRefresher);
+			} catch (Exception e1) {
+				throw new RuntimeException(e1);
+			}
+		}
+	}
+
+}
---
+++ branches/release-1.3.0-RC1/src/test/java/ca/sqlpower/wabit/swingui/StubWabitSwingSession.java
@@ -0,0 +1,181 @@
+/*
+ * Copyright (c) 2008, SQL Power Group Inc.
+ *
+ * This file is part of Wabit.
+ *
+ * Wabit is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Wabit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+package ca.sqlpower.wabit.swingui;
+
+import java.beans.PropertyChangeListener;
+import java.io.File;
+import java.net.URI;
+import java.sql.Connection;
+import java.util.List;
+
+import javax.swing.JTree;
+
+import ca.sqlpower.sql.DataSourceCollection;
+import ca.sqlpower.sql.JDBCDataSource;
+import ca.sqlpower.sql.SPDataSource;
+import ca.sqlpower.swingui.SPSwingWorker;
+import ca.sqlpower.swingui.db.DatabaseConnectionManager;
+import ca.sqlpower.swingui.event.SessionLifecycleListener;
+import ca.sqlpower.util.UserPrompter;
+import ca.sqlpower.util.UserPrompter.UserPromptOptions;
+import ca.sqlpower.util.UserPrompter.UserPromptResponse;
+import ca.sqlpower.util.UserPrompterFactory.UserPromptType;
+import ca.sqlpower.wabit.StubWabitSession;
+import ca.sqlpower.wabit.WabitSession;
+import ca.sqlpower.wabit.WabitWorkspace;
+import ca.sqlpower.wabit.swingui.tree.WorkspaceTreeModel;
+
+/**
+ * This stubbed swing session does nothing for all of its methods.
+ */
+public class StubWabitSwingSession implements WabitSwingSession {
+	
+	WabitSwingSessionContext context = new StubWabitSwingSessionContext();
+	private WabitSession delegateSession;
+	
+	public StubWabitSwingSession() {
+		delegateSession = new StubWabitSession(context);
+		
+		// XXX leaking a reference to partially-constructed session!
+		delegateSession.getWorkspace().setSession(this);
+	}
+
+	public WabitSwingSessionContext getContext() {
+		return context;
+	}
+
+	public void addSessionLifecycleListener(
+			SessionLifecycleListener<WabitSession> l) {
+		//Do nothing
+	}
+
+	public boolean close() {
+		//Do nothing
+		return false;
+	}
+
+	public void removeSessionLifecycleListener(
+			SessionLifecycleListener<WabitSession> l) {
+		//Do nothing
+	}
+
+	public WabitWorkspace getWorkspace() {
+		return delegateSession.getWorkspace();
+	}
+	
+	public DatabaseConnectionManager getDbConnectionManager() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public UserPrompter createUserPrompter(String question, UserPromptType responseType,
+			UserPromptOptions optionType, UserPromptResponse defaultResponseType, Object defaultResponse,
+			String ... buttonNames) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void addPropertyChangeListener(PropertyChangeListener l) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void removePropertyChangeListener(PropertyChangeListener l) {
+		// TODO Auto-generated method stub
+		
+	}
+
+    public Connection borrowConnection(JDBCDataSource dataSource) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public JTree getTree() {
+        return new JTree();
+    }
+
+    public WorkspaceTreeModel getWorkspaceTreeModel() {
+        return new WorkspaceTreeModel(getWorkspace()); // XXX new tree model? this looks fishy!
+    }
+
+    public DataSourceCollection<SPDataSource> getDataSources() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+	public UserPrompter createDatabaseUserPrompter(String question,
+			List<Class<? extends SPDataSource>> dsTypes,
+			UserPromptOptions optionType,
+			UserPromptResponse defaultResponseType, Object defaultResponse,
+			DataSourceCollection<SPDataSource> dsCollection,
+			String... buttonNames) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+    public File getCurrentURIAsFile() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public URI getCurrentURI() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void setCurrentURI(URI uri) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void registerSwingWorker(SPSwingWorker worker) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void removeSwingWorker(SPSwingWorker worker) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public boolean hasUnsavedChanges() {
+        return false;
+    }
+
+    public void runInBackground(Runnable runner) {
+        runner.run();
+    }
+
+    public void runInForeground(Runnable runner) {
+        runner.run();
+    }
+    
+    public boolean isForegroundThread() {
+    	return true;
+    }
+
+	public boolean isEnterpriseServerSession() {
+		return this.delegateSession.isEnterpriseServerSession();
+	}
+
+	public WabitWorkspace getSystemWorkspace() {
+		return this.delegateSession.getSystemWorkspace();
+	}
+}
